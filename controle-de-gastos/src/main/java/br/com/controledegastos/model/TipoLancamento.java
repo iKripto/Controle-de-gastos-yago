44d73c74cca64e22663c4540dc4e9d1ccc2c084b
iKripto <yagosiqueira450@gmail.com>
2025-09-09 20:22:18 -0300

    primeira etapa pronta

---
+++ controle-de-gastos/src/main/java/br/com/controledegastos/model/TipoLancamento.java
@@ -0,0 +1,9 @@
+public // src/main/java/br/com/controledegastos/model/TipoLancamento.java
+package br.com.controledegastos.model;
+
+public enum TipoLancamento {
+    RECEITA,
+    DESPESA
+} TipoLancamento {
+    
+}
