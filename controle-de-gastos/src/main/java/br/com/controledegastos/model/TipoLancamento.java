1d02020ae81a69398709401386539c53b3e614ff
iKripto <yagosiqueira450@gmail.com>
2025-09-09 20:26:33 -0300

    Correção no TipoLancamento.Java

--- controle-de-gastos/src/main/java/br/com/controledegastos/model/TipoLancamento.java
+++ controle-de-gastos/src/main/java/br/com/controledegastos/model/TipoLancamento.java
@@ -1,9 +1,6 @@
-public // src/main/java/br/com/controledegastos/model/TipoLancamento.java
 package br.com.controledegastos.model;
 
 public enum TipoLancamento {
     RECEITA,
     DESPESA
-} TipoLancamento {
-    
 }
